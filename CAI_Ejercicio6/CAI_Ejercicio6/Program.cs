c036894b14116d7ce9ba8414acfca07d7c0dafba
michellerosenblat <70615029+michellerosenblat@users.noreply.github.com>
2020-09-04 16:05:22 -0300

    Ejercicio6
    Se resuelve el ejercicio 6

---
+++ CAI_Ejercicio6/CAI_Ejercicio6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0;
+            int numero2 = 0;
+            int resultado = 0;
+            Console.WriteLine("Ingrese el primer numero");
+
+            while (!Int32.TryParse(Console.ReadLine(), out numero1)) {
+                Console.WriteLine("Ingrese nuevamente el primer numero");
+            }
+            Console.WriteLine(numero1);
+
+            Console.WriteLine("Ingrese el segundo numero");
+            while (!Int32.TryParse(Console.ReadLine(), out numero2))
+            {
+                Console.WriteLine("Ingrese nuevamente el segundo numero");
+            }
+
+            resultado = (numero1 + numero2) * (numero1 - numero2);
+
+            Console.WriteLine("El resultado es " + resultado);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CAI_Ejercicio8/CAI_Ejercicio8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1;
+            DateTime d2;
+
+            Console.WriteLine("Ingrese la primera fecha");
+
+            while (!(DateTime.TryParse(Console.ReadLine (), out d1))){ 
+            Console.WriteLine("Ingrese de nuevo la primera fecha");
+            }
+            Console.WriteLine("Ingrese la segunda fecha");
+
+            while (!(DateTime.TryParse(Console.ReadLine(), out d2))){
+                Console.WriteLine("Ingrese de nuevo la segunda fecha");
+            }
+
+            Console.WriteLine((d2 - d1).TotalDays);
+
+            Console.ReadLine();
+
+        }
+    }
+}
