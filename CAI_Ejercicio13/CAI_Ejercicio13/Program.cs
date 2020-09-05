e58eafc186c771c1f7cbd0e233aba29001c48669
michellerosenblat <70615029+michellerosenblat@users.noreply.github.com>
2020-09-05 19:17:29 -0300

    ej13

---
+++ CAI_Ejercicio13/CAI_Ejercicio13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            
+            while (true) {
+                int res;
+                Console.WriteLine("Ingrese un numero");
+                int.TryParse(Console.ReadLine(), out num);
+                if (num > 0) { 
+                List<int> numeros = Enumerable.Range(1, num-1).ToList();
+                res = num;
+                foreach (int numero in numeros)
+                {
+                    res = res * numero;
+                }
+                }
+                else
+                {
+                    res = 0;
+                }
+                Console.WriteLine("Aca tenes el factorial " + res);
+            }
+        }
+    }
+}
