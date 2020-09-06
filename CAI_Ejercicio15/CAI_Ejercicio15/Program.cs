dea15bea0da03f0fc3c387276eca6d379b272dec
michellerosenblat <70615029+michellerosenblat@users.noreply.github.com>
2020-09-06 15:06:21 -0300

    ej15

---
+++ CAI_Ejercicio15/CAI_Ejercicio15/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string password ="michelle";
+            
+            while (true) {
+            int i = 1;
+            bool correcta = false;
+                do
+                {
+                    Console.WriteLine("Ingrese la contraseña");
+                    if (Console.ReadLine() == password)
+                    {
+                        Console.WriteLine("Contraseña correcta");
+                        correcta = true;
+                    
+                    }
+                    else
+                    {
+                        if (i == 3)
+                        {
+                            Console.WriteLine("Clave bloqueada");
+                        }
+                        else { 
+                        Console.WriteLine("Acceso denegado. La contraseña no es correcta");
+                            i++;
+                        }
+                    }
+                }
+                while (i <= 3 && correcta ==false);
+                }
+        }
+    }
+    }
+
