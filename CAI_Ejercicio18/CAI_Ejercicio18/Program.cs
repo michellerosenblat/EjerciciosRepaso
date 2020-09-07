536eec60fb227875a766347c784ddf3ee2b13cac
michellerosenblat <70615029+michellerosenblat@users.noreply.github.com>
2020-09-07 12:13:56 -0300

    ej 18

---
+++ CAI_Ejercicio18/CAI_Ejercicio18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Ejercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nro;
+            string hexa;
+            string octal;
+            string binario;
+            
+            do
+            {
+                Console.WriteLine("Ingrese un numero");
+            }
+            while (!int.TryParse(Console.ReadLine(), out nro));
+
+            binario = ConvertirABinario(nro);
+            hexa = ConvertirAHexadecimal(nro);
+            octal= ConvertirAOctal(nro);
+
+            Console.WriteLine("En binario es: " + binario);
+            Console.WriteLine("En hexadecimal es: " + hexa);
+            Console.WriteLine("En octal es: " + octal);
+            Console.ReadLine();
+
+            
+        }
+
+        static string ConvertirAHexadecimal (int valor) {
+            return valor.ToString("X");
+        }
+        static string ConvertirABinario(int valor)
+        {
+            return Convert.ToString(valor, 2);
+        }
+        static string ConvertirAOctal(int valor)
+        {
+            return Convert.ToString(valor, 8);
+        }
+    }
+}
